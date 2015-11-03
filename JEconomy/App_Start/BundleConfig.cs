b754017ae1696bff541c8acdafff7570a70c6056
Sealion <patrik.sjolin@gmail.com>
2015-11-03 08:33:43 +0100

    Adding sort to table

--- JEconomy/App_Start/BundleConfig.cs
+++ JEconomy/App_Start/BundleConfig.cs
@@ -21,14 +21,16 @@ namespace JEconomy
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-table.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootbox").Include(
                 "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-table.css"));
         }
     }
 }
